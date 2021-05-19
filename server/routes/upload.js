76e78f7be34434abde202d90871a45346bd04a2e
xia0m <alexma325@gmail.com>
2021-05-19 08:13:51 -0700

    removed unnecessary comments

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const {uploadPhotos} = require('../controllers/upload');
-// const {check_max_photos} = require('../middleware/upload');
 
-// const {check_max_photos} = require('../middleware/upload')
 
 const storage = multer.memoryStorage({
 	destination: function(req,file,callback){
