991498a878285d94cae3dc252a553a6767ca2ae3
Gursharan Singh <gursharansingh4312@gmail.com>
2021-05-19 13:40:36 -0700

    request controllers added

---
+++ server/controllers/request.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+
+const Request = require('../models/Request');
+
+// @route GET /requests
+// @desc Get all requests of current users
+// @access Private
+exports.getRequests = asyncHandler(async (req,res,next) => {
+    try {
+        const requests = await Request.find({user: req.user.id}).populate('setter');
+        res.status(200).json({requests});
+      } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+      }
+})
+
+// @route POST /requests
+// @desc Create new request for the user
+// @access Private
+exports.postRequest = asyncHandler(async (req,res,next) => {
+    const {setter,start,end} = req.body;
+    const newRequest = new Request({
+        user: req.user.id,
+		setter,
+		start,
+		end
+    });
+
+    try {
+        await newRequest.save();
+        res.status(201).json({request:newRequest});
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+      }
+})
+
+// @route UPDATE /requests
+// @desc update request details
+// @access Private
+exports.updateRequest = asyncHandler(async (req,res,next) => {
+	const id = req.params.id;
+    const {setter,start,end, accepted, declined, paid } = req.body;
+
+    try {
+	  const request = await Request.findById(id);
+
+	  //checking if request is updated by same user
+	  if(request.user.equals(req.user.id)){
+		// changing only feilds given by the user
+		request.setter = setter? setter: request.setter;
+		request.start = start? start: request.start;
+		request.end = end? end: request.end;
+		request.accepted = accepted? accepted: request.accepted;
+		request.declined = declined? declined: request.declined;
+		request.paid = paid? paid: request.paid;
+
+		await request.save();
+		res.status(200).json({request});
+	  }else{
+		res.status(400);
+		throw new Error("Request not found");
+	  }
+    } catch (error) {
+      res.status(500);
+      throw new Error(error.message);
+    }
+})
---
+++ server/routes/request.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const protect = require('../middleware/auth');
+const {getRequests,postRequest,updateRequest} = require('../controllers/request')
+
+router.route("/").get(protect,getRequests).post(protect,postRequest);
+router.route("/:id").patch(protect, updateRequest);
+
+
+module.exports = router;
