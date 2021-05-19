4e3b0998acc432e881fd7271a8e75e3f328df098
xia0m <alexma325@gmail.com>
2021-05-19 01:44:48 -0700

    finished photos upload to s3

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const {uploadPhotos} = require('../controllers/upload');
+// const {check_max_photos} = require('../middleware/upload');
 
-router.route('/upload').get((req,res)=>{
-    res.send('good')
-})
+// const {check_max_photos} = require('../middleware/upload')
+
+const storage = multer.memoryStorage({
+	destination: function(req,file,callback){
+		callback(null,'')
+	}
+});
+
+const MAX_PHOTO_SIZE = 5;
+const FOLDER_NAME = 'photos';
+
+const multipleUpload = multer({storage:storage})
+	.array(FOLDER_NAME,MAX_PHOTO_SIZE);
+
+router.route('/upload').post(
+	multipleUpload, uploadPhotos
+);
 
 module.exports = router;
