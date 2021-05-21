9904f9d13802d34446fe2a4db8f7f04cc48655ae
Gursharan Singh <gursharansingh4312@gmail.com>
2021-05-21 11:47:19 -0700

    request model and controller changed

--- server/controllers/request.js
+++ server/controllers/request.js
@@ -6,63 +6,60 @@ const Request = require('../models/Request');
 // @route GET /requests
 // @desc Get all requests of current users
 // @access Private
-exports.getRequests = asyncHandler(async (req,res,next) => {
-    try {
-        const requests = await Request.find({user: req.user.id}).populate('sitter');
-        res.status(200).json({requests});
-      } catch (error) {
-        res.status(500);
-        throw new Error(error.message);
-      }
-})
+exports.getRequests = asyncHandler(async (req, res, next) => {
+  try {
+    const requests = await Request.find({ $or: [{ user: req.user.id }, { sitter: req.user.id }] })
+      .populate('sitter', '-password')
+      .populate('user', '-password');
+
+    requests.map((request) => {
+      if (request.user._id == req.user.id) request.user = undefined;
+      else request.sitter = undefined;
+      return request;
+    });
+
+    res.status(200).json({ requests });
+  } catch (error) {
+    res.status(500);
+    throw new Error(error.message);
+  }
+});
 
 // @route POST /requests
 // @desc Create new request for the user
 // @access Private
-exports.postRequest = asyncHandler(async (req,res,next) => {
-    const {sitter,start,end} = req.body;
-    const newRequest = new Request({
-        user: req.user.id,
-		sitter,
-		start,
-		end
-    });
+exports.postRequest = asyncHandler(async (req, res, next) => {
+  const { sitter, start, end } = req.body;
+  if (!sitter) {
+    res.status(400);
+    throw new Error('No sitter provided');
+  }
+  const newRequest = new Request({
+    user: req.user.id,
+    sitter,
+    start,
+    end,
+  });
 
-    try {
-        await newRequest.save();
-        res.status(201).json({request:newRequest});
-    } catch (error) {
-        res.status(500);
-        throw new Error(error.message);
-      }
-})
+  try {
+    await newRequest.save();
+    res.status(201).json({ request: newRequest });
+  } catch (error) {
+    res.status(500);
+    throw new Error(error.message);
+  }
+});
 
 // @route UPDATE /requests
 // @desc update request details
 // @access Private
-exports.updateRequest = asyncHandler(async (req,res,next) => {
-	const id = req.params.id;
-    const {sitter,start,end, accepted, declined, paid } = req.body;
-    try {
-	  const request = await Request.findById(id);
-	  //checking if request is updated by same user
-	  if(request.user.equals(req.user.id)){
-		// changing only fields given by the user
-		request.sitter = sitter? sitter: request.sitter;
-		request.start = start? start: request.start;
-		request.end = end? end: request.end;
-		request.accepted = accepted? accepted: request.accepted;
-		request.declined = declined? declined: request.declined;
-		request.paid = paid? paid: request.paid;
-
-		await request.save();
-		res.status(200).json({request});
-	  }else{
-		res.status(400);
-		throw new Error("Request not found");
-	  }
-    } catch (error) {
-      res.status(500);
-      throw new Error(error.message);
-    }
-})
+exports.updateRequest = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const request = await Request.findOneAndUpdate(id, { $set: req.body }, { new: true });
+    res.status(200).json({ request });
+  } catch (error) {
+    res.status(500);
+    throw new Error(error.message);
+  }
+});
--- server/routes/request.js
+++ server/routes/request.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/auth');
-const {getRequests,postRequest,updateRequest} = require('../controllers/request')
+const { getRequests, postRequest, updateRequest } = require('../controllers/request');
 
-router.route("/").get(protect,getRequests).post(protect,postRequest);
-router.route("/:id").patch(protect, updateRequest);
-
+router.route('/').get(protect, getRequests);
+router.route('/').post(protect, postRequest);
+router.route('/:id').patch(protect, updateRequest);
 
 module.exports = router;
