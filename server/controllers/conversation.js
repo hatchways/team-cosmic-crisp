76fe2d41b584bccdf3ae53aee7a7a3aa43f3a0ba
Rajiv Titus <rajivtitus92@gmail.com>
2021-06-01 06:54:27 -0700

    Modified conversation controller

--- server/controllers/conversation.js
+++ server/controllers/conversation.js
@@ -23,7 +23,8 @@ exports.getConversations = asyncHandler(async (req,res,next) => {
               conversationId: conversation._id,
               recipient: await Profile.findById(
                 conversation.participants.filter(participant => {
-                  return participant !== user.profile._id;
+                  console.log(participant)
+                  return participant !== user.profile._id.toString();
                 })[0]
                 ).select("firstName lastName profilePhoto")
               }
