eb6418e945a09677c33129e61f90b6c470aac022
xia0m <alexma325@gmail.com>
2021-06-01 11:18:23 -0700

    added basic dropdown style for notification

--- client/src/components/Notification/Notification.tsx
+++ client/src/components/Notification/Notification.tsx
@@ -1,12 +1,37 @@
-import { Button } from '@material-ui/core';
+import { useState } from 'react';
+import { Button, ClickAwayListener } from '@material-ui/core';
 import useStyles from './useStyles';
 import useNavBarStyles from '../Navbar/useStyles';
 
 export default function Notification(): JSX.Element {
   const classes = { ...useStyles(), ...useNavBarStyles() };
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
   return (
-    <Button variant="text" className={classes.userNavItem}>
-      Notifications <span className={classes.active} />
-    </Button>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <span className={classes.root}>
+        <Button variant="text" className={`${classes.userNavItem}`} onClick={handleClick}>
+          Notifications <span className={classes.active} />
+        </Button>
+        {open ? (
+          <div className={classes.dropdown}>
+            <ul>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+              <li>A</li>
+            </ul>
+          </div>
+        ) : null}
+      </span>
+    </ClickAwayListener>
   );
 }
--- client/src/components/Notification/useStyles.ts
+++ client/src/components/Notification/useStyles.ts
@@ -1,5 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+  },
+  dropdown: {
+    position: 'absolute',
+    top: 30,
+    right: -20,
+    userSelect: 'none',
+    border: '1px solid #c7d8e2',
+    padding: theme.spacing(1),
+    backgroundColor: '#fff',
+    width: 400,
+    height: 'auto',
+    borderRadius: 4,
+    boxShadow: '5px 5px 15px rgba(0,0,0,0.125), -5px -5px 15px rgba(0,0,0,0.125)',
+  },
+}));
 
 export default useStyles;
