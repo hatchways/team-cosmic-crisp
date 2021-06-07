de08736143cee39bdf67d525cce3f483ae0b1091
Gursharan Singh <gursharansingh4312@gmail.com>
2021-06-07 12:08:41 -0700

    emojis support added

--- client/src/pages/Messages/ActiveChat/Input/Input.tsx
+++ client/src/pages/Messages/ActiveChat/Input/Input.tsx
@@ -1,6 +1,9 @@
-import { FormControl, FilledInput } from '@material-ui/core';
+import { FormControl, FilledInput, TextField, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './useStyles';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, MouseEvent, useState, useRef, useEffect } from 'react';
+import Picker, { IEmojiData } from 'emoji-picker-react';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import SendIcon from '@material-ui/icons/Send';
 
 interface Props {
   handleSendMessage: (text: string) => void;
@@ -9,29 +12,69 @@ interface Props {
 const Input = ({ handleSendMessage }: Props): JSX.Element => {
   const classes = useStyles();
   const [text, setText] = useState<string>('');
+  const [emojiPicker, setEmojiPicker] = useState<boolean>(false);
+  const emojiContainerRef = useRef<HTMLDivElement | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setText(e.target.value);
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement> | MouseEvent) => {
+    console.log('submit');
     e.preventDefault();
     handleSendMessage(text);
     setText('');
   };
 
+  const onEmojiClick = (event: MouseEvent<Element, globalThis.MouseEvent>, emojiObject: IEmojiData) => {
+    setText(text + emojiObject.emoji);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event: Event) {
+      if (
+        event.target instanceof HTMLElement &&
+        emojiContainerRef.current &&
+        !emojiContainerRef.current.contains(event.target)
+      ) {
+        setEmojiPicker(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [emojiContainerRef]);
+
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
-      <FormControl fullWidth hiddenLabel>
-        <FilledInput
-          classes={{ root: classes.input }}
-          disableUnderline
-          placeholder="Type something..."
-          value={text}
-          name="text"
-          onChange={handleChange}
-        />
-      </FormControl>
+      <Grid className={`${classes.emojiPicker} ${!emojiPicker ? classes.hideEmoji : ''}`} ref={emojiContainerRef}>
+        <Picker onEmojiClick={onEmojiClick} />
+      </Grid>
+
+      <TextField
+        className={classes.input}
+        variant="outlined"
+        placeholder="Type something..."
+        value={text}
+        name="text"
+        onChange={handleChange}
+        InputProps={{
+          endAdornment: (
+            <>
+              <Button onClick={() => setEmojiPicker(!emojiPicker)}>
+                <EmojiEmotionsIcon />
+              </Button>
+              <Button type="submit" onClick={handleSubmit}>
+                <SendIcon />
+              </Button>
+            </>
+          ),
+        }}
+      />
     </form>
   );
 };
--- client/src/pages/Messages/ActiveChat/Input/useStyles.ts
+++ client/src/pages/Messages/ActiveChat/Input/useStyles.ts
@@ -4,13 +4,21 @@ const useStyles = makeStyles((theme) => ({
   root: {
     justifySelf: 'flex-end',
     marginTop: 15,
+    width: '100%',
+    position: 'relative',
   },
   input: {
-    height: 70,
+    width: '100%',
     backgroundColor: '#F4F6FA',
-    borderRadius: 8,
-    marginBottom: 20,
-    border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: '8px 8px 0 0',
+  },
+  emojiPicker: {
+    position: 'absolute',
+    bottom: '120%',
+    right: '0',
+  },
+  hideEmoji: {
+    display: 'none',
   },
 }));
 
