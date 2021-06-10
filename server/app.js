65ff357630e638ce30e682a3a6741ed42b1acfd9
Gursharan Singh <gursharansingh4312@gmail.com>
2021-06-10 09:38:49 -0700

    app js changes

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use('/messages', messageRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html'));
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html')));
 } else {
   app.get('/', (req, res) => {
     res.send('API is running');
